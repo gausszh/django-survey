4250cdac5161648f61513c5c00aa7e1d8560c0e2
zhangxiaorun <zxrn0403@corp.netease.com>
2016-07-30 18:38:04 +0800

    转为中文化， 以及优化一些UI

--- survey/settings.py
+++ survey/settings.py
@@ -1,6 +1,6 @@
 # Django settings for survey project.
 
-import os 
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -26,6 +26,7 @@ DATABASES = {
     }
 }
 
+SITE_ID = 1
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -33,24 +34,33 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Los_Angeles'
+# TIME_ZONE = 'America/Los_Angeles'
+TIME_ZONE = 'Asia/Hong_Kong'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-cn'
 
-SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
+# USE_I18N = True
 USE_I18N = True
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale.
+# USE_L10N = True
 USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
+# USE_TZ = True
+USE_TZ = False
+
+
+
+
+
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
@@ -59,7 +69,7 @@ MEDIA_ROOT = path("../media/")
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -115,7 +125,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    path("../templates/")
+    path("../templates/"),
 )
 
 INSTALLED_APPS = (
@@ -130,7 +140,7 @@ INSTALLED_APPS = (
     'survey',
 )
 
-support_email = "survey@coliving.org"
+support_email = "1150177351@qq.org"
 
 
 # A sample logging configuration. The only tangible logging
